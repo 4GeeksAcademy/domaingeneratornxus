5b7bb9faefc89ee6f83b5c23afeba07b08217ff1
Nxusbets <169100845+Nxusbets@users.noreply.github.com>
2024-07-11 03:24:28 +0000

    domain

---
+++ src/domain.js
@@ -0,0 +1,20 @@
+let pronombre = ['the', 'our'];
+let adjetivo = ['great', 'big'];
+let sujeto = ['jogger', 'racoon'];
+
+pronombre.forEach(articulo => {
+    
+    adjetivo.forEach(adj => {
+        sujeto.forEach(suj => {
+            
+            console.log(articulo + adj + suj + ".com");
+         });
+    });
+
+
+
+});
+
+
+
+
